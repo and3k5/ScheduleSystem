a44d0fb74beec900185282b02ea827e4a57a7f3d
Anders K <ande7353@gmail.com>
2014-12-03 14:50:36 +0100

    Use entityframework

---
+++ ScheduleSystem/ScheduleSystem.Data/ScheduleSystem.cs
@@ -0,0 +1,35 @@
+namespace ScheduleSystem.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ScheduleSystem : DbContext
+    {
+        // Your context has been configured to use a 'ScheduleSystem' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'ScheduleSystem.Data.ScheduleSystem' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'ScheduleSystem' 
+        // connection string in the application configuration file.
+        public ScheduleSystem()
+            : base("name=ScheduleSystem.DB")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Lecture> Lectures { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
