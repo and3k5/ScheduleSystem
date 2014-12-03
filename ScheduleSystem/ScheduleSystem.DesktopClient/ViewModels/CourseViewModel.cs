1e02fe03b94e5a830f6f1c05bb3d36795c82ba87
Anders K <ande7353@gmail.com>
2014-12-03 14:16:31 +0100

    CourseViewModel and dialog

---
+++ ScheduleSystem/ScheduleSystem.DesktopClient/ViewModels/CourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleSystem.DesktopClient.ViewModels
+{
+    class CourseViewModel : Course, ViewModel
+    {
+        public override string Validate(string fieldname) {
+            if (fieldname == "Name")
+            {
+                return "Hej";
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
