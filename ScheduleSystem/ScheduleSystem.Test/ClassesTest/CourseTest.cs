1e0a890d4d5ffb97d026579edf7cde47d7a2e52c
Anders K <ande7353@gmail.com>
2014-12-03 14:14:52 +0100

    Course test changes

--- ScheduleSystem/ScheduleSystem.Test/ClassesTest/CourseTest.cs
+++ ScheduleSystem/ScheduleSystem.Test/ClassesTest/CourseTest.cs
@@ -9,34 +9,58 @@ namespace ScheduleSystem.Test.ClassesTest
         [TestMethod]
         public void CheckIfStudensAreAddedToCourse()
         {
+            // Create course and student
             Course course = new Course();
             Student student = new Student();
+
+            // Set some information in student object
             student.CPR = "010101-0101";
             student.Name = "Herbert";
             student.Email = "test@test.com";
+
+            // Add student to course object
             course.Students.Add(student);
+
+            // Assertion = course should contain one student
             Assert.IsTrue(course.Students.Count == 1);
         }
 
         [TestMethod]
         public void CheckIfStudensAreRemovedFromCourse()
         {
+            // Create course and student
             Course course = new Course();
             Student student = new Student();
+
+            // Set some information in student object
             student.CPR = "010101-0101";
             student.Name = "Herbert";
             student.Email = "test@test.com";
+            
+            // Add student to course object
             course.Students.Add(student);
-            int oldCount = course.Students.Count;
+
+            // Assertion = course should contain one student
+            Assert.IsTrue(course.Students.Count == 1);
+            
+            // Remove student from course object
             course.Students.Remove(student);
-            Assert.IsTrue((course.Students.Count == 0) && (oldCount == 1));
+
+            // Assertion = course should not contain any student
+            Assert.IsTrue(course.Students.Count == 0);
         }
 
         [TestMethod]
         public void CheckIfNameIsAssignedFromConstructor()
         {
-            Course course = new Course("WAT");
-            Assert.AreEqual("WAT", course.Name);
+            // Set course name variable
+            String CourseName = "Programming 3 - Test";
+            
+            // Create course with name assigned from contructor parameter
+            Course course = new Course(CourseName);
+            
+            // Assertion = course name should match the constructor parameter
+            Assert.AreEqual(CourseName, course.Name);
         }
     }
 }
