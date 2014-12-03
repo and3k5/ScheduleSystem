a87358693d503e7b0b39649ad23518383e65a80a
Voxvalve <rene.viper999@gmail.com>
2014-12-03 13:27:07 +0100

    Added Lecture TestMethod

--- ScheduleSystem/ScheduleSystem.Test/ClassesTest/LectureTest.cs
+++ ScheduleSystem/ScheduleSystem.Test/ClassesTest/LectureTest.cs
@@ -7,8 +7,24 @@ namespace ScheduleSystem.Test.ClassesTest
     public class LectureTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckIfTeacherIsAssignedLecture()
         {
+            Lecture lecture = new Lecture();
+            Teacher teacher = new Teacher();
+
+            teacher.Name = "Michael";
+            teacher.Email = "Mich@mich.com";
+            teacher.Skills = "Much_skills";
+
+            DateTime date1 = new DateTime(2014, 5, 1, 8, 30, 52);
+            DateTime date2 = new DateTime(2014, 5, 2, 8, 30, 52);
+
+            lecture.Name = "Programming";
+            lecture.StartDate = date1;
+            lecture.EndDate = date2;
+            lecture.Teachers.Add(teacher);
+
+            Assert.IsTrue(lecture.Teachers.Count == 1);
         }
     }
 }
