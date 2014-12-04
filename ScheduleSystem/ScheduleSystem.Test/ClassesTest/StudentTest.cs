067732cc129173c8343f490bfd21647f82433407
Voxvalve <rene.viper999@gmail.com>
2014-12-04 09:40:28 +0100

    More UnitTests

--- ScheduleSystem/ScheduleSystem.Test/ClassesTest/StudentTest.cs
+++ ScheduleSystem/ScheduleSystem.Test/ClassesTest/StudentTest.cs
@@ -9,6 +9,30 @@ namespace ScheduleSystem.Test.ClassesTest
         [TestMethod]
         public void TestMethod1()
         {
+            /* 1 - Create new student
+             * 2 - Add student name
+             * 3 - Add student Email
+             * 4 - Add student CPR
+             * 5 - Check if student has been created
+             * 6 - Check student name
+             * 7 - Check student Email
+             * 8 - Check student CPR
+             **/
+
+            string sName = "Carmen Electra";
+            string sEmail = "Carmen@Dolls.com";
+            string sCpr = "";
+
+            Student student = new Student();
+
+            student.Name = sName;
+            student.Email = sEmail;
+            student.CPR = sCpr;
+
+            Assert.IsTrue(student != null);
+            Assert.IsTrue(student.Name == sName);
+            Assert.IsTrue(student.Email == sEmail);
+            Assert.IsTrue(student.CPR == sCpr);
         }
     }
 }
