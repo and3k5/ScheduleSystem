0e84b5a57a63272a83b42559ae7ebe009d0f22a9
Anders K <ande7353@gmail.com>
2014-12-09 11:50:51 +0100

    Comments on CourseDialog

--- ScheduleSystem/ScheduleSystem.DesktopClient/Views/CourseDialog.xaml.cs
+++ ScheduleSystem/ScheduleSystem.DesktopClient/Views/CourseDialog.xaml.cs
@@ -25,66 +25,82 @@ namespace ScheduleSystem.DesktopClient.Views
         {
             InitializeComponent();
         }
-        private CourseViewModel SSCTX
-        {
-            get
-            {
-                return (CourseViewModel)DataContext;
-            }
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            StudentDialog studiag = new StudentDialog();
-            studiag.Show();
-        }
 
-        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        private void CommonCmd_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
+            // User can always add or edit a customer, so can execute will just be
+            // true for both of the commands
             e.CanExecute = true;
         }
 
-        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        private void EditCmd_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            // This function below is not best practice when looking from 
+            // the MVVM perspective.
+            // We've prioritised functionality and setup above MVVM principles.
+
+            // Fetch the student which is 'owner' of the fired 'Edit' command
             Student student = (Student)e.Parameter;
+
+            // Create a copy element which is linked to the dialog below.
+            // We won't link the real Student object to the viewmodel, 
+            // because of the option to Cancel changes (press X on window)
             Student copy = new Student();
             copy.Name = student.Name;
             copy.Email = student.Email;
             copy.CPR = student.CPR;
+
+            // Create Student dialog with StudentModelView linked to the Student copy
             StudentDialog sDialog = new StudentDialog();
             sDialog.DataContext = new StudentViewModel(copy);
-            sDialog.Closed += sDialog_Closed;
+
+            // Get result of the dialog (Did the user press X or Save?)
             Nullable<bool> result = sDialog.ShowDialog();
+
+            // If save was pressed, apply the specified data to the real student
             if (result == true)
             {
                 student.Name = copy.Name;
                 student.Email = copy.Email;
                 student.CPR = copy.CPR;
             }
+
+            // Also this is far from MVVM standard, but PropertyChanged
+            // does not work properly with the DataGrid item.
             UpdateItems();
-            //sDialog.Show();
         }
 
         private void NewCmd_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            // This function below is not best practice when looking from 
+            // the MVVM perspective.
+            // We've prioritised functionality and setup above MVVM principles.
+
+            // Create a new Student element which would be directly linked
+            // to the viewmodel
             Student student = new Student();
+
+            // Create dialog and link the ViewModel
             StudentDialog sDialog = new StudentDialog();
             sDialog.DataContext = new StudentViewModel(student);
-            sDialog.Closed += sDialog_Closed;
+
+            // Get result of the dialog (Did the user press X or Save?)
             Nullable<bool> result = sDialog.ShowDialog();
+
+            // If save was pressed, Add the student object to the database
             if (result == true)
             {
-                SSCTX.Students.Add(student);
+                ((CourseViewModel)DataContext).Students.Add(student);
             }
+
+            // Also this is far from MVVM standard, but PropertyChanged
+            // does not work properly with the DataGrid item.
             UpdateItems();
-            //sDialog.Show();
         }
         private void UpdateItems()
         {
+            // Update items on the datagrid (the list of students)
             dataGrid.Items.Refresh();
-        }
-        void sDialog_Closed(object sender, EventArgs e)
-        {
-            UpdateItems();
         }
     }
 }
