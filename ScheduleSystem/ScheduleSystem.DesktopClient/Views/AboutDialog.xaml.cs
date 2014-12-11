1e3423815b52474e2550b0a1f1d2acd528a573e4
Anders K <ande7353@gmail.com>
2014-12-11 12:16:51 +0100

    Invisible update

--- ScheduleSystem/ScheduleSystem.DesktopClient/Views/AboutDialog.xaml.cs
+++ ScheduleSystem/ScheduleSystem.DesktopClient/Views/AboutDialog.xaml.cs
@@ -21,11 +21,65 @@ namespace ScheduleSystem.DesktopClient.Views
         private DispatcherTimer timer = new DispatcherTimer();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            timer.Interval = TimeSpan.FromMilliseconds(60);
+            aboutString += fakeErrString("System information:");
+            aboutString += "\n" + fakeErrString(Environment.OSVersion.VersionString);
+            aboutString += "\n" + fakeErrString("Logged in as: \n      " + Environment.UserName);
+            aboutString += "\n" + fakeErrString("Computername: \n      " + Environment.MachineName);
+            timer.Interval = TimeSpan.FromMilliseconds(35);
             timer.Tick += timer_Tick;
             timer.Start();
         }
-        private String aboutString = "ScheduleSystem!1\b\nMdae\b\b\bade bt\by:\nRene\bê\bë\bé & Andre\b\bers\n\nA schoolprojekt\b\bct for Ma\bercantek\bc Viborg.\b, Da\benmark!";
+        private string fakeErrString(string original)
+        {
+            string newstring = "";
+            int n;
+            Random rnd = new Random();
+            for (int i = 0, len = original.Length; i < len; i++)
+            {
+                string outp = original[i].ToString();
+                int v = rnd.Next(0, 8);
+                if (v > 6)
+                {
+                    bool number = Char.IsNumber(original[i]);
+                    bool letter = Char.IsLetter(original[i]);
+                    if (letter)
+                    {
+                        
+                        int code = new Random().Next(65, 65 + 25);
+                        char newChar = (char)code;
+                        if (Char.IsUpper(original[i]))
+                        {
+                            newChar = Char.ToUpper(newChar);
+                        }
+                        else
+                        {
+                            newChar = Char.ToLower(newChar);
+                        }
+                        outp = newChar.ToString() + "\b" + original[i].ToString();
+                    }
+                    else if (number)
+                    {
+                        
+                        int num;
+                        bool r = int.TryParse(original[i].ToString(), out num);
+                        if (r)
+                        {
+                            Random rnd2 = new Random();
+                            int newNumber = rnd2.Next(0, 9);
+                            while (newNumber == num)
+                            {
+                                newNumber = rnd2.Next(0, 9);
+                            }
+                            outp = newNumber.ToString() + "\b" + original[i].ToString();
+                        }
+                    }
+                }
+                
+                newstring += outp;
+            }
+            return newstring;
+        }
+        private String aboutString = "ScheduleSystem!1\b\nMdae\b\b\bade bt\by:\nRene\bê\bë\bé & Andre\b\bers\n\nA schoolprojekt\b\bct for Ma\bercantek\bc Viborg.\b, Da\benmark!\n\n";
         int cursor = 0;
         void timer_Tick(object sender, EventArgs e)
         {
@@ -37,7 +91,8 @@ namespace ScheduleSystem.DesktopClient.Views
                     Span span = new Span();
                     Run run = new Run("");
                     span.Inlines.Add(run);
-                    span.FontSize = 15;
+                    span.FontSize = 13;
+                    p.LineHeight = 1;
                     p.Inlines.Add(span);
                     RTBox.Document.Blocks.Add(p);
                 }
@@ -66,7 +121,7 @@ namespace ScheduleSystem.DesktopClient.Views
                 {
                     if ((aboutString[cursor] == '\b') && (aboutString[cursor - 1] != '\b'))
                     {
-                        timer.Interval = TimeSpan.FromMilliseconds(100 + new Random().Next(0, 30));
+                        timer.Interval = TimeSpan.FromMilliseconds(35 + new Random().Next(0, 80));
                     }
                 }
             }
